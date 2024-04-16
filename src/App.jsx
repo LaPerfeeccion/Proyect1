1730aa77b5afe1083f14abc99cfe5ee895d28fca
LaPerfeecion <compiteamigos@gmail.com>
2024-04-15 20:38:17 -0500

    ayudaplss

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,25 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Send from './components/send'
+import { Stack, Typography,Container } from '@mui/material'
+import Text from './components/text'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1> yo </h1>
+    <Stack className='formulario'>
+      <Typography variant='h1'> 
+          Login 
+      </Typography>
+      <Container className='textfield'>
+      <Text></Text>
+         </Container>
+      <Send> </Send>
+    </Stack>
+      
     </>
   )
 }
