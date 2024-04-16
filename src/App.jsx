89b4bfb053379f9ff68c006dcdf6c55795e04bce
NinoTorres611 <oeta611@gmail.com>
2024-04-15 21:52:23 -0400

    update 3.4

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 import Send from './components/send'
 import { Stack, Typography,Container } from '@mui/material'
 import Text from './components/text'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
